04287e699afc8c38690377926ba217cb5f9c18e4
Alex Johnson <me@alex-johnson.net>
2016-02-20 05:18:24 -0500

    added hank's template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,96 @@
-<html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+@extends("app")
+@section("content")
+<!-- Header Carousel -->
+<header id="myCarousel" class="carousel slide">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+	</ol>
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
-
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner">
+		<div class="item active">
+			<div class="fill" style="background-image:url('Guitar%201.jpg');"></div>
+			<div class="carousel-caption">
+				<h1>Create</h1>
 			</div>
 		</div>
-	</body>
-</html>
+		<div class="item">
+			<div class="fill" style="background-image:url('Guitar2.jpg');"></div>
+			<div class="carousel-caption">
+				<h1>Inspire</h1>
+			</div>
+		</div>
+		<div class="item">
+			<div class="fill" style="background-image:url('electric-guitar.jpg');"></div>
+			<div class="carousel-caption">
+				<h1>Electrify</h1>
+			</div>
+		</div>
+	</div>
+
+	<!-- Controls -->
+	<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		<span class="icon-prev"></span>
+	</a>
+	<a class="right carousel-control" href="#myCarousel" data-slide="next">
+		<span class="icon-next"></span>
+	</a>
+</header>
+
+<!-- Page Content -->
+<div class="container">
+
+	<!-- Marketing Icons Section -->
+	<div class="row">
+		<div class="col-lg-12">
+			<h1 class="page-header">
+				Master of the Chords
+			</h1>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4><i class="fa fa-fw fa-check-circle"></i> New Song</h4>
+				</div>
+				<div class="panel-body">
+					<p>Create a song of your own! Put in a few notes and begin your Masterpiece!</p>
+					<a href="#" class="btn btn-default">Learn More</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4><i class="fa fa-fw fa-home"></i> Sign in</h4>
+				</div>
+				<div class="panel-body">
+					<p>Already a master of the chords? Sign in now to keep building your legacy!</p>
+					<a href="#" class="btn btn-default">Learn More</a>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4><i class="fa fa-fw fa-thumbs-up"></i> New User?</h4>
+				</div>
+				<div class="panel-body">
+					<p>Create an account to save your creations!</p>
+					<a href="#" class="btn btn-default">Learn More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+<!-- Script to Activate the Carousel -->
+<script>
+	$('.carousel').carousel({
+		interval: 5000 //changes the speed
+	})
+</script>
+<!-- /.container -->
+@endsection
