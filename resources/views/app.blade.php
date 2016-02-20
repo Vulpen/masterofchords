a349282d4ca96d75871c8a2c3e2978c7f752d076
shottyshotts <shotts.caleb@gmail.com>
2016-02-20 17:09:10 -0500

    adding haikusgit add resources

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -46,6 +46,14 @@
                 <li>
                     <a href="/contact">Contact</a>
                 </li>
+                <li>
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                </li>
             </ul>
         </div>
         <!-- /.navbar-collapse -->
